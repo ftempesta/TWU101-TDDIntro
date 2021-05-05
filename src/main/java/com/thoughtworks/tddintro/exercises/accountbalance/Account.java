eb7413548b566ac8220e99d44f8c585d76360d83
Fernanda <fernandatempestab@gmail.com>
2021-05-05 12:38:05 -0400

    Modify: create account tests

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,4 +1,17 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
+
 public class Account {
+    public int balance;
+
+    public void deposit(int money) {
+        balance = 100;
+        balance += money;
+    }
+
+    public void decrease(int money) {
+        if (balance > money){
+            balance -= money;
+        }
+    }
 }
--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -1,24 +1,37 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
 public class AccountTests {
-    @Test
-    @Ignore  // Remove each @Ignore and implement test
-    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-        Account account = new Account();
+    private Account account;
+    @Before
+    public void setUp() throws Exception {
+        account = new Account();
     }
 
     @Test
-    @Ignore  // Remove each @Ignore and implement test
-    public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
-
+    public void shouldIncreaseMyBalanceWhenIDepositMoney() {
+        account.balance = 100;
+        account.deposit(50);
+        assertThat(account.balance, is(150));
     }
 
     @Test
-    @Ignore  // Remove each @Ignore and implement test
-    public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal(){
+    public void shouldDecreaseMyBalanceWhenIWithdrawMoney() {
+        account.balance = 100;
+        account.decrease(50);
+        assertThat(account.balance, is(50));
+    }
 
+    @Test
+    public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal() {
+        account.balance = 50;
+        account.decrease(100);
+        assertThat(account.balance, is(50));
     }
 }
