b95d198baa20307ef71ec7054cda3293ee6a91cc
lafernanda.tempesta <lafernanda.tempesta@lafernandatempestas-MacBook-Pro.local>
2021-05-05 11:34:36 -0400

    Passing tests

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,6 +2,13 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        return -1;
+        if (i < 0) {
+            throw new IllegalArgumentException("Negative number!");
+        }
+        int fact = 1;
+        for (int j = 1; j <= i; j ++) {
+            fact *= j;
+        }
+        return fact;
     }
 }
